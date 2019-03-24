591b5933c3b05ac9786bfa3bf94d6cdf07fe3f95
Chansol, Han <chansol0505@naver.com>
2019-03-24 22:15:11 +0900

    init filenames

--- dogdoor.c
+++ dogdoor.c
@@ -16,9 +16,12 @@
 
 MODULE_LICENSE("GPL");
 
-char user_uid[128] = { 0x0, } ;
+#define FILENAME_COUNT 10;
+
+char user_uid[128] = "-1";
 void ** sctable ;
-uid_t username_uid = -1;
+char * accessed_filenames[FILENAME_COUNT];
+int count_accessed_filenames = 0;
 
 asmlinkage int (*orig_sys_open)(const char __user * filename, int flags, umode_t mode) ; 
 
@@ -78,6 +81,28 @@ int file_sync(struct file *file)
     return 0;
 }
 
+void print_filenames() {
+    int i;
+    for (i=0; i<FILENAME_COUNT; i++) {
+        if (accessed_filenames[i] != NULL) {
+            printk("ACCESSED FILE (%d) : %s\n", i, accessed_filenames[i]);
+        }
+    }
+}
+
+void init_filenames(const char __user * filename) {
+    if (count_accessed_filenames < 10) {
+        accessed_filenames[count_accessed_filenames] = filename;
+        count_accessed_filenames++;
+    } else {
+        int i;
+        for (i=0; i<9; i++) {
+            accessed_filenames[i] = accessed_filenames[i+1];
+        }
+        accessed_filenames[9] = filename;
+    }
+}
+
 asmlinkage int dogdoor_sys_open(const char __user * filename, int flags, umode_t mode)
 {
 	char fname[256] ;
@@ -85,27 +110,40 @@ asmlinkage int dogdoor_sys_open(const char __user * filename, int flags, umode_t
 	copy_from_user(fname, filename, 256) ;
 
     const struct cred * my_cred = current_cred();
-    long i_user_uid = simple_strtol(user_uid, NULL, 10);
+    int i_user_uid = (int)simple_strtol(user_uid, NULL, 10);
 
-    if (my_cred->uid.val == (unsigned int)i_user_uid && i_user_uid != -1) {
-        struct file * ftest = file_open("/var/log/test.log", O_WRONLY|O_CREAT, 0644);
-        file_write(ftest, 0, "testtest", 8);
-        file_close(ftest);
+    if (my_cred->uid.val == i_user_uid && i_user_uid != -1) {
+        /*struct file * ftest = file_open("/var/log/test.log", O_WRONLY|O_CREAT, 0644);*/
+        /*file_write(ftest, 0, "testtest", 8);*/
+        /*file_close(ftest);*/
+        init_filenames(filename);
         printk("FILE: %s, Current UID: %d, Current EUID: %d\n", filename, my_cred->uid, my_cred->euid);
+        print_filenames();
     }
 
 	return orig_sys_open(filename, flags, mode) ;
 }
 
 
-static 
-int dogdoor_proc_open(struct inode *inode, struct file *file) {
-	return 0 ;
-}
+static
+ssize_t dogdoor_log_proc_read(struct file *file, char __user *ubuf, size_t size, loff_t *offset) 
+{
+	ssize_t toread ;
 
-static 
-int dogdoor_proc_release(struct inode *inode, struct file *file) {
-	return 0 ;
+    for(i=0; i<FILENAME_COUNT; i++) {
+        if (accessed_filenames[i] != NULL){
+            toread = strlen(accessed_filenames[i]) >= *offset + size ? size : strlen(accessed_filenames[i]) - *offset ;
+            if (copy_to_user(ubuf, accessed_filenames[i]+ *offset, toread))
+                return -EFAULT ;	
+            *offset = *offset + toread ;
+        }
+    }
+
+	/*toread = strlen(buf) >= *offset + size ? size : strlen(buf) - *offset ;*/
+	/*if (copy_to_user(ubuf, buf + *offset, toread))*/
+		/*return -EFAULT ;	*/
+	/**offset = *offset + toread ;*/
+	return toread ;
 }
 
 static
@@ -121,6 +159,18 @@ ssize_t dogdoor_proc_read(struct file *file, char __user *ubuf, size_t size, lof
 		return -EFAULT ;	
 	*offset = *offset + toread ;
 	return toread ;
+}
+
+static 
+ssize_t dogdoor_log_proc_write(struct file *file, const char __user *ubuf, size_t size, loff_t *offset) 
+{
+	if (*offset != 0 || size > 128)
+		return -EFAULT ;
+
+	if (copy_from_user(buf, ubuf, size))
+		return -EFAULT ;
+
+	return *offset ;
 }
 
 static 
@@ -140,13 +190,25 @@ ssize_t dogdoor_proc_write(struct file *file, const char __user *ubuf, size_t si
 	return *offset ;
 }
 
+static int proc_open(struct inode *inode, struct file *file) { return 0 ; }
+static int proc_release(struct inode *inode, struct file *file) { return 0 ; }
+
+static const struct file_operations dogdoor_log_fops = {
+	.owner = 	THIS_MODULE,
+	.open = 	proc_open,
+	.read = 	dogdoor_log_proc_read,
+	.write = 	dogdoor_log_proc_write,
+	.llseek = 	seq_lseek,
+	.release = 	proc_release,
+} ;
+
 static const struct file_operations dogdoor_fops = {
 	.owner = 	THIS_MODULE,
-	.open = 	dogdoor_proc_open,
+	.open = 	proc_open,
 	.read = 	dogdoor_proc_read,
 	.write = 	dogdoor_proc_write,
 	.llseek = 	seq_lseek,
-	.release = 	dogdoor_proc_release,
+	.release = 	proc_release,
 } ;
 
 static 
@@ -155,6 +217,7 @@ int __init dogdoor_init(void) {
 	pte_t * pte ;
 
 	proc_create("dogdoor", S_IRUGO | S_IWUGO, NULL, &dogdoor_fops) ;
+	proc_create("dogdoor_log", S_IRUGO | S_IWUGO, NULL, &dogdoor_log_fops) ;
 
 	sctable = (void *) kallsyms_lookup_name("sys_call_table") ;
 
@@ -172,6 +235,7 @@ void __exit dogdoor_exit(void) {
 	unsigned int level ;
 	pte_t * pte ;
 	remove_proc_entry("dogdoor", NULL) ;
+	remove_proc_entry("dogdoor_log", NULL) ;
 
 	sctable[__NR_open] = orig_sys_open ;
 	pte = lookup_address((unsigned long) sctable, &level) ;
