0637695f70fe2bf45a3c35d0d97984e4a900b9ca
Chansol, Han <chansol0505@naver.com>
2019-03-24 21:30:14 +0900

    write file test

--- dogdoor.c
+++ dogdoor.c
@@ -6,9 +6,13 @@
 #include <linux/kallsyms.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <asm/unistd.h>
 #include <linux/cred.h>
 #include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/buffer_head.h>
+#include <asm/unistd.h>
+#include <asm/segment.h>
+#include <asm/uaccess.h>
 
 MODULE_LICENSE("GPL");
 
@@ -17,6 +21,62 @@ void ** sctable ;
 uid_t username_uid = -1;
 
 asmlinkage int (*orig_sys_open)(const char __user * filename, int flags, umode_t mode) ; 
+
+struct file *file_open(const char *path, int flags, int rights) 
+{
+    struct file *filp = NULL;
+    mm_segment_t oldfs;
+    int err = 0;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+    filp = filp_open(path, flags, rights);
+    set_fs(oldfs);
+    if (IS_ERR(filp)) {
+        err = PTR_ERR(filp);
+        return NULL;
+    }
+    return filp;
+}
+
+void file_close(struct file *file) 
+{
+        filp_close(file, NULL);
+}
+
+int file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
+{
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+
+    ret = vfs_read(file, data, size, &offset);
+
+    set_fs(oldfs);
+    return ret;
+}  
+
+int file_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
+{
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+
+    ret = vfs_write(file, data, size, &offset);
+
+    set_fs(oldfs);
+    return ret;
+}
+
+int file_sync(struct file *file) 
+{
+    vfs_fsync(file, 0);
+    return 0;
+}
 
 asmlinkage int dogdoor_sys_open(const char __user * filename, int flags, umode_t mode)
 {
@@ -28,7 +88,10 @@ asmlinkage int dogdoor_sys_open(const char __user * filename, int flags, umode_t
     long i_user_uid = simple_strtol(user_uid, NULL, 10);
 
     if (my_cred->uid.val == (unsigned int)i_user_uid && i_user_uid != -1) {
-        printk("OS Homework : %s, Current UID: %d, Current EUID: %d, Current USER: %s\n", fname, my_cred->uid, my_cred->euid, my_cred->user);
+        struct file * ftest = file_open("/var/log/test.log", O_WRONLY|O_CREAT, 0644);
+        file_write(ftest, 0, "testtest", 8);
+        file_close(ftest);
+        printk("FILE: %s, Current UID: %d, Current EUID: %d\n", filename, my_cred->uid, my_cred->euid);
     }
 
 	return orig_sys_open(filename, flags, mode) ;
